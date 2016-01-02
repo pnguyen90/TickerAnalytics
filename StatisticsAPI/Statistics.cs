71d5ac324b4049b9f49f5e470ec676b3e26d7ee1
Phong Nguyen <phovft@gmail.com>
2016-01-02 12:04:33 -0800

    filled out distance and centroid methods

--- StatisticsAPI/Statistics.cs
+++ StatisticsAPI/Statistics.cs
@@ -65,18 +65,37 @@ namespace StatisticsAPI
         }
 
         //returns the centroid of an array of (x,y) coordinates. For internal use.
-        private static Tuple<decimal,decimal> centroid(Tuple<decimal,decimal> coordinates)
+        private static Tuple<decimal,decimal> centroid(Tuple<decimal,decimal>[] coordinates)
         {
-            Tuple<decimal,decimal> sample = new Tuple<decimal,decimal>(5,5);
-            return sample;
+            decimal average_x;
+            decimal average_y;
+            decimal sum_x = 0;
+            decimal sum_y = 0;
+            decimal length = Convert.ToDecimal(coordinates.Length);
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                sum_x += coordinates[i].Item1;
+                sum_y += coordinates[i].Item2;
+            }
+            average_x = sum_x / length;
+            average_y = sum_y / length;
+            Tuple<decimal,decimal> result = new Tuple<decimal,decimal>(average_x,average_y);
+            return result;
+
+            
 
         }
 
         //returns the distance between two coordinates. For internal use.
         private static decimal distance(Tuple<decimal,decimal> point1, Tuple<decimal,decimal> point2)
         {
-            decimal sample = 5;
-            return sample;
+            decimal x_diff = point2.Item1 - point1.Item1;
+            decimal y_diff = point2.Item2 - point1.Item1;
+            decimal squared_distance = x_diff * x_diff + y_diff * y_diff;
+            double squared_distance_d = double(squared_distance);
+            double distance_d = Math.Sqrt(squared_distance_d);
+            decimal distance = Convert.ToDecimal(distance_d);
+            return distance;
         }
 
     }
